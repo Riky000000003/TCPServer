492734ecca006f4ffb5126fe1dca233d268945c7
Riccardo.Frigerio <134052922+Riky000000003@users.noreply.github.com>
2023-06-15 17:51:16 +0200

    .

--- src/main/java/org/example/MyServer.java
+++ src/main/java/org/example/MyServer.java
@@ -1,13 +1,62 @@
 package org.example;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 /**
  * Hello world!
  *
  */
 public class MyServer
 {
+    static int portNumber = 1234;
     public static void main( String[] args )
     {
+        ServerSocket serverSocket;
         System.out.println( "Hello World!" );
+        try {
+             serverSocket = new ServerSocket(portNumber);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        Socket clientSocket;
+        try {
+            clientSocket = serverSocket.accept();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        System.out.println("Accept");
+
+        BufferedReader in = null;
+        try {
+            in = new BufferedReader(
+                    new InputStreamReader(clientSocket.getInputStream()));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        PrintWriter out = null; // allocate to write answer to client.
+        try {
+            out = new PrintWriter(clientSocket.getOutputStream(), true);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        String s = "";
+        try {
+            while ((s = in.readLine()) != null) {
+                System.out.println(s);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+            out.println(s.toUpperCase());
+        }
     }
 }
